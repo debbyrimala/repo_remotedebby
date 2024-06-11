d990cc61b45715acddb93213d344d9ae9da3a826
debbyrimalaputri <debbyrimala@gmail.com>
2024-06-11 15:08:39 +0700

    Menambahkan File index2.php

---
+++ index2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index 2</title>
+</head>
+<body>
+    <h1>Latihan Remote Github - GIT</h1><br>
+    <h1>Kelas 22 MI B</h1>
+</body>
+</html>
